c08f82dcaa538df0865366e425c2750e645df199
johncecilio <40663480+johncecilio@users.noreply.github.com>
2020-08-03 14:49:20 +0800

    Update app.js

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,7 +9,7 @@ app.get('/mars', function (req, res) {
   res.send('Hello Mars!\n');
 });
 
-app.listen(9090, function () {
-  console.log('Example app listening on port 9090!');
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
 });
 
